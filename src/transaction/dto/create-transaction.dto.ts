31e8c9ab0d8b9ea9bb58b3806155d2efcf0fa5a3
Ivan-Adamovich <ivanadams1998@gmail.com>
2024-02-28 01:23:57 +0300

    Add transactions CRUD API

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,22 @@
-export class CreateTransactionDto {}
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator'
+import { Category } from 'src/category/entities/category.entity'
+import { User } from 'src/user/entities/user.entity'
+
+export class CreateTransactionDto {
+	@IsNotEmpty()
+	@IsString()
+	title: string
+
+	@IsNotEmpty()
+	@IsNumber()
+	amount: number
+
+	@IsString()
+	type: 'expense' | 'income'
+
+	@IsNotEmpty()
+	category: Category
+
+	@IsOptional()
+	user: User
+}
